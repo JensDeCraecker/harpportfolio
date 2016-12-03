88aa34af98d1da93ef1a6449930839beec581299
JensDeCraecker <jens.decraecker@student.ap.be>
2016-12-03 16:28:24 +0100

    Almost done

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,5 +1,12 @@
 function main() {
-          // hier komt je code
+            $(function() {
+              $('.js-nav a, .js-connect').click(function(e) {
+                e.preventDefault();
+                $('body, html').animate({
+                  scrollTop: $($.attr(this, 'href')).offset().top
+                }, 750);
+              });
+            });
 }
 
 window.onload = function() {
